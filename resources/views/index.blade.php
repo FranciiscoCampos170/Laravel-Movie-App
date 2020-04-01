68355810120c19d842456387052e20c48341a855
Francisco Campos <franciiscocampos170@gmail.com>
2020-04-01 21:47:34 +0100

    configure API

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,175 +8,28 @@
                 Popular Movies
             </h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                <div class="mt-8">
+                @foreach ($popularMovies as $movie)
+                    <div class="mt-8">
                     <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path']}}" alt="poster    " class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
+                    <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $movie['title']}}</a>
                         <div class="flex items-center text-gray-400 text-sm mt-1">
                             <span>star</span>
-                            <span class="ml-1">85%</span>
+                        <span class="ml-1">{{ $movie['vote_average'] * 10 . '%'}}</span>
                             <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
+                            <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y')}}</span>
                         </div>
                         <div class="text-gray-400 text-sm">
                             Action, Thiller, Comedy
                         </div>
                     </div>
                 </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
+
+                @endforeach
+
+
                 </div>
             </div>
             <!-- end of popular movies -->
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::resource('/', 'MoviesController');
 
