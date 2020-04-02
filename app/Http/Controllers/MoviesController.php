693a7efb246e19de5e3c87ee4a8fb668d40c3e1d
Francisco Campos <franciiscocampos170@gmail.com>
2020-04-02 12:39:14 +0100

    popular movies finished

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -18,14 +18,27 @@ class MoviesController extends Controller
                         ->get('https://api.themoviedb.org/3/movie/popular')
                         ->json()['results'];
 
+        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
+        ->get('https://api.themoviedb.org/3/movie/now_playing')
+        ->json()['results'];
+
+
         $genresArray = Http::withToken(config('services.tmdb.token'))
                         ->get('https://api.themoviedb.org/3/genre/movie/list')
-                        ->json()['results'];
+                        ->json()['genres'];
+
+        $genres = collect($genresArray)->mapWithKeys(function ($genre){
+            return [
+                $genre['id'] => $genre['name']
+            ];
+        });
 
 
-                        dump($popularMovies);
+        dump($nowPlayingMovies);
         return view('index', [
             'popularMovies' => $popularMovies,
+            'nowPlayingMovies' => $nowPlayingMovies,
+            'genres' => $genres
         ]);
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
             </h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                 @foreach ($popularMovies as $movie)
+                    
                     <div class="mt-8">
                     <a href="#">
                     <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path']}}" alt="poster    " class="hover:opacity-75 transition ease-in-out duration-150">
@@ -22,7 +23,12 @@
                             <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y')}}</span>
                         </div>
                         <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
+                             @foreach ($movie['genre_ids'] as $genre)
+                                {{ $genres->get($genre) }}
+                                @if (!$loop->last)
+                                    ,
+                                @endif
+                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -57,74 +63,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpeg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
-                        <div class="flex items-center text-gray-400 text-sm mt-1">
-                            <span>star</span>
-                            <span class="ml-1">85%</span>
-                            <span class="mx-2">|</span>
-                            <span>Feb 20, 2020</span>
-                        </div>
-                        <div class="text-gray-400 text-sm">
-                            Action, Thiller, Comedy
-                        </div>
-                    </div>
-                </div>
+            </div>
 
             <!-- end of now playing movies -->
         </div>
